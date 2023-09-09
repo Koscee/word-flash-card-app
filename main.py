37773f3a5f8bca3d186dcc5d5253295976302c9c
Koscee <52253099+Koscee@users.noreply.github.com>
2023-09-09 23:07:08 +0800

    feat: add flipping of cards

--- main.py
+++ main.py
@@ -4,27 +4,43 @@ from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+words_data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+
 from_lang = "French"
 to_lang = "English"
-
-words_data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+current_word = choice(words_data)
 
 
 def next_card():
-    canvas.itemconfig(card_title, text=from_lang)
-    canvas.itemconfig(card_word, text=choice(words_data)[from_lang])
+    global current_word, flip_timerId
+    window.after_cancel(flip_timerId)
+    current_word = choice(words_data)
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    canvas.itemconfig(card_title, text=from_lang, fill="black")
+    canvas.itemconfig(card_word, text=current_word[from_lang], fill="black")
+    flip_timerId = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back_img)
+    canvas.itemconfig(card_title, text=to_lang, fill="white")
+    canvas.itemconfig(card_word, text=current_word[to_lang], fill="white")
+    window.after_cancel(flip_timerId)
 
 
 window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timerId = window.after(3000, func=flip_card)
+
 # create canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text=from_lang, font=("Arial", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="trouve", font=("Arial", 60, "bold"))
+card_word = canvas.create_text(400, 263, text=current_word[from_lang], font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # create buttons
